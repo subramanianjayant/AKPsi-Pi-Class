66ff18d30459f32ed6890d6bb47bbfff4a0ba666
Jayant Subramanian <jrs2287@columbia.edu>
2019-03-03 14:31:46 -0500

    adding 50d MA

--- indices.py
+++ indices.py
@@ -7,12 +7,15 @@ def get_soup(url):
 	return BeautifulSoup(urlopen(url),'html.parser')
 
 def get_stats(ticker):
-	url = 'https://www.cnbc.com/quotes/?symbol=.'+ticker
-	soup = get_soup(url)
-	price = soup.find(itemprop = 'price').get('content')
-	price_chng = soup.find(itemprop = 'priceChange').get('content')
-	price_pct = soup.find(itemprop = 'priceChangePercent').get('content')
-	return (str(ticker+':\t'+str(price)+' | '+str(price_chng)+' | '+str(price_pct)))
+	#url = 'https://www.cnbc.com/quotes/?symbol=.'+ticker
+        url = 'https://www.barchart.com/stocks/quotes/$'+ticker+'/technical-analysis'
+        soup = get_soup(url)
+        price = soup.find('span',{'data-ng-class_':'highlightValue(lastPrice)'}).getText()
+	#price_chng = soup.find(itemprop = 'priceChange').get('content')
+	#price_pct = soup.find(itemprop = 'priceChangePercent').get('content')
+	price_chng = soup.find('span',{'data-ng-class_':'highlightValue(priceChange)'}).getText()
+        price_pct = soup.find('span',{'data-ng-class_':'highlightValue(percentChange)'}).getText()
+        return (str(ticker+':\t'+str(price)+' | '+str(price_chng)+' | '+str(price_pct)))
 
 stats = [get_stats(ticker) for ticker in tickers]
 
